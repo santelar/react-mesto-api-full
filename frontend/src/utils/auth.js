a9eb561629ebffcde31e164a17b40deead6d2960
Alexander Ponomarenko <madi-al@yandex.ru>
2021-04-11 15:33:59 +0300

    change back url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,6 +1,6 @@
-//export const baseUrl = 'https://api.santelar.nomoredomains.icu';
+export const baseUrl = 'https://api.santelar.nomoredomains.icu';
 
-export const baseUrl = 'http://localhost:3001';
+//export const baseUrl = 'http://localhost:3001';
 
 const checkResponse = (res) => {
   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
